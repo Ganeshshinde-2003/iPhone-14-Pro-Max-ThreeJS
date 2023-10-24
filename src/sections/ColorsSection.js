acfdeab6b58675c91e5638f9c87200302046b820
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-10-24 20:22:12 +0530

    created

--- src/sections/ColorsSection.js
+++ src/sections/ColorsSection.js
@@ -1,4 +1,5 @@
-import React from "react";
+import gsap from "gsap"; // Make sure to import gsap from the correct path
+import React, { useLayoutEffect, useRef } from "react";
 import styled from "styled-components";
 
 const Section = styled.section`
@@ -13,14 +14,12 @@ const Left = styled.div`
   width: 50%;
   height: 100vh;
   display: flex;
-  background-color: rgba(155, 181, 206, 0.8);
   position: relative;
 `;
 const Right = styled.div`
   width: 50%;
   height: 100vh;
   display: flex;
-  background-color: rgba(155, 181, 206, 0.4);
   position: relative;
 `;
 const Center = styled.div`
@@ -32,14 +31,95 @@ const Center = styled.div`
   transform: translate(-50%, -50%) rotate(-90deg);
   font-size: var(--fontxxl);
   text-transform: uppercase;
+  filter: brightness(0.85);
 `;
 
 const ColorsSection = () => {
+  const sectionRef = useRef(null);
+  const rightRef = useRef(null);
+  const leftRef = useRef(null);
+  const textRef = useRef(null);
+
+  useLayoutEffect(() => {
+    let Elem = sectionRef.current;
+    let rightElem = rightRef.current;
+    let leftElem = leftRef.current;
+    let textElem = textRef.current;
+
+    let updateColor = (color, text, rgbColor) => {
+      textElem.innerText = text;
+      textElem.style.color = color;
+      rightElem.style.backgroundColor = `rgba(${rgbColor}, 0.4)`;
+      leftElem.style.backgroundColor = `rgba(${rgbColor}, 0.8)`;
+    };
+
+    gsap.to(Elem, {
+      scrollTrigger: {
+        trigger: Elem,
+        start: "top top",
+        end: `+=${Elem.offsetWidth + 1000}`,
+        scrub: 1,
+        pin: true,
+        pinSpacing: true,
+      },
+    });
+
+    let t2 = gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: Elem,
+          start: "top top",
+          end: `+=${Elem.offsetWidth + 1000}`,
+          scrub: 1,
+        },
+      })
+      .to(Elem, {
+        onStart: updateColor,
+        onStartParams: ["#9BB5CE", "Sierra Blue", "155, 181, 206"],
+        onReverseComplete: updateColor,
+        onReverseCompleteParams: ["#9BB5CE", "Sierra Blue", "155, 181, 206"],
+      })
+      .to(Elem, {
+        onStart: updateColor,
+        onStartParams: ["#F9E5C9", "Gold", "249, 229, 201"],
+        onReverseComplete: updateColor,
+        onReverseCompleteParams: ["#F9E5C9", "Gold", "249, 229, 201"],
+      })
+      .to(Elem, {
+        onStart: updateColor,
+        onStartParams: ["#505F4E", "Alpine Green", "80, 95, 78"],
+        onReverseComplete: updateColor,
+        onReverseCompleteParams: ["#505F4E", "Alpine Green", "80, 95, 78"],
+      })
+      .to(Elem, {
+        onStart: updateColor,
+        onStartParams: ["#574f6f", "Deep Purple", "87, 79, 111"],
+        onReverseComplete: updateColor,
+        onReverseCompleteParams: ["#574f6f", "Deep Purple", "87, 79, 111"],
+      })
+      .to(Elem, {
+        onStart: updateColor,
+        onStartParams: ["#A50011", "Red", "165, 0, 17"],
+        onReverseComplete: updateColor,
+        onReverseCompleteParams: ["#A50011", "Red", "165, 0, 17"],
+      })
+      .to(Elem, {
+        onStart: updateColor,
+        onStartParams: ["#215E7C", "Blue", "33, 94, 124"],
+        onReverseComplete: updateColor,
+        onReverseCompleteParams: ["#215E7C", "Blue", "33, 94, 124"],
+      });
+
+    return () => {
+      // if (t2) t2.kill();
+    };
+  }, []);
+
   return (
-    <Section>
-      <Left></Left>
-      <Center>Sierra Blur</Center>
-      <Right></Right>
+    <Section ref={sectionRef}>
+      <Left ref={leftRef}></Left>
+      <Center ref={textRef}>Sierra Blue</Center>
+      <Right ref={rightRef}></Right>
     </Section>
   );
 };
